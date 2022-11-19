803a817d787e710d8279ef289367c80a22704e70
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-11-18 21:22:11 -0500

    commit tree 26bf5c29d673c43e24c7fa0b73fec70b7579448f

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+function reverse(word) {
+  // 'abc' => 'cba'
+  // const wordArray = word.split("")
+  // const reversedWordArray = wordArray.reverse()
+  // const reversedWord = reversedWordArray.join("");
+  // return reversedWord;
+  // code above can be refractored down to just the below
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  // reverse the string
+  const  reversedWord = reverse(word)
+
+  // if the input is the same as the reversed input
+  // if(word === reversedWord) {
+  //   return true
+  // } else {
+  //   return false
+  // }
+  // code above is redundant and can be simplified to just the below
+  return word === reversedWord
 }
 
 /* 
   Add your pseudocode here
+  mom === mom // return true
+  abc === cba // return false
+
+  make a function that returns true if a word is a palindrome and false if not.
+  that means if the word is the same as the word in reverse, I should return true.
+
+  reverse the string
+
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
 /*
